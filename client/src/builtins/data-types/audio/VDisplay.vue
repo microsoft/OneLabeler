afd3600ab2e8c92df9318f4ea85f463aec7e04fb
Yu Zhang <yuzhang94@outlook.com>
2021-06-22 22:27:01 +0800

    feat(client): add basic waveform and spectrogram support

--- client/src/builtins/data-types/audio/VDisplay.vue
+++ client/src/builtins/data-types/audio/VDisplay.vue
@@ -1,16 +1,29 @@
 <template>
-  <VMedia
-    ref="media"
-    component="audio"
-    :src="dataObject.content"
-    :width="width"
-    :height="height"
-    @timeupdate="onTimeUpdate"
-  />
+  <div
+    :style="{
+      'width': widthStr,
+      'height': heightStr,
+      'display': 'flex',
+      'flex-direction': 'column',
+    }"
+  >
+    <div style="flex: 1 1 auto; display: flex; flex-direction: column;">
+      <div ref="waveSpectrum" />
+      <div ref="waveform" style="flex: 1 1 auto" />
+    </div>
+    <VMedia
+      ref="media"
+      component="audio"
+      :src="dataObject.content"
+      @timeupdate="onTimeUpdate"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue, { PropType } from 'vue';
+import WaveSurfer from 'wavesurfer.js';
+import SpectrogramPlugin from 'wavesurfer.js/src/plugin/spectrogram';
 import { IVideo } from '@/commons/types';
 import VMedia from '../video/VMedia.vue';
 
@@ -42,9 +55,48 @@ export default Vue.extend({
       },
     },
   },
+  data() {
+    return {
+      wavesurfer: null as WaveSurfer | null,
+    };
+  },
+  computed: {
+    widthStr(): string {
+      const { width } = this;
+      if (typeof width === 'number') return `${width}px`;
+      return width;
+    },
+    heightStr(): string {
+      const { height } = this;
+      if (typeof height === 'number') return `${height}px`;
+      return height;
+    },
+  },
+  mounted() {
+    const media = this.getMedia();
+    media.volume = 0.2;
+    this.registerWaveSurfer();
+  },
   methods: {
     onTimeUpdate(e: Event): void {
       this.$emit('timeupdate', e);
+      if (this.wavesurfer === null) return;
+      this.wavesurfer.setCurrentTime(this.getMedia().currentTime);
+    },
+    registerWaveSurfer(): void {
+      const media = this.getMedia();
+
+      // TODO: make the size of waveform and spectrogram responsive to the window size.
+      const wavesurfer = WaveSurfer.create({
+        height: 256,
+        container: this.$refs.waveform as HTMLElement,
+        plugins: [SpectrogramPlugin.create({
+          container: this.$refs.waveSpectrum as HTMLElement,
+          fftSamples: 1024,
+        })],
+      });
+      wavesurfer.load(media);
+      this.wavesurfer = wavesurfer;
     },
     getMedia(): HTMLMediaElement {
       const component = this.$refs.media as Vue & {
